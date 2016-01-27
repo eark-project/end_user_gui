51f38666f1bfa2ebafc76f290975f3c23997e0e0
Beemen Beshara <beemen@magenta-aps.dk>
2016-01-27 12:49:12 +0100

    New data class: ArchiveMetadata

---
+++ Models/ArchiveMetadata.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace end_user_gui.Models
+{
+    /// <summary>
+    /// TODO: Fill this from EAD content in /metadata.xml file (if available)
+    /// </summary>
+    public class ArchiveMetadata
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
