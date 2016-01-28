ba10ee34bb7b13dcf825fc16c5a42ad0f54794af
Beemen Beshara <beemen@magenta-aps.dk>
2016-01-28 12:53:53 +0100

    Auto generated code for OrdersController in OMT

--- OMT/App_Start/RouteConfig.cs
+++ OMT/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace OMT
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "OMT.Controllers" }
             );
         }
     }
