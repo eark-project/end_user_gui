27acacff0c38940064e2de81847a34635f00e4ae
beemen <beshara_beemen@hotmail.com>
2016-02-05 13:38:08 +0100

    Even more isolation

--- OMT/App_Start/BundleConfig.cs
+++ OMT/App_Start/BundleConfig.cs
@@ -15,6 +15,10 @@ namespace OMT
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle( "~/bundles/select.dropdown").Include(
+                "~/Scripts/select.dropdown.js"
+                ));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ OMT/Scripts/select.dropdown.js
@@ -0,0 +1,20 @@
+﻿function containerSpanId(elm) {
+    var span = elm;
+    while (span.tagName.toLowerCase() != 'span')
+        span = span.parentElement;
+    elementId = span.getAttribute('id');
+    return elementId;
+}
+
+function updateSelectSaveLink(select, column) {
+
+    var elementId = containerSpanId(select)
+    var a = $('#' + elementId + ' a[save]');
+    if (!a.attr('orgHref')) {
+        a.attr('orgHref', a.attr('href'));
+    }
+    a.attr(
+        'href',
+        a.attr('orgHref') + '&' + column + '=' + select.value + '&set=true'
+    );
+}
